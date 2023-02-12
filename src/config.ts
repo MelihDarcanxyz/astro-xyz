0edf57560526e64a3fd5649113dd5b9ba8e2acd6
Melih Darcan <57872471+MelihDarcanxyz@users.noreply.github.com>
2023-02-12 11:01:25 +0300

    Boilerplate

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Site config
+export const SITE_TITLE = "My Website";
+export const SITE_DESCRIPTION = ""
+
+// Author config
+export const AUTHOR = "John Doe";
+export const EMAIL = "john_doe@examplemail.com"
