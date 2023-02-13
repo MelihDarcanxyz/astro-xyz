103abe0c6f017e8cdaca46e7a699d708feba44b7
Melih Darcan <57872471+MelihDarcanxyz@users.noreply.github.com>
2023-02-13 15:26:37 +0300

    mdx, rss, icon, sitemap and robots.txt support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-    site: "https://example.org/"
+  site: "https://example.org/",
+  integrations: [mdx(), robotsTxt(), sitemap()]
 });
