6a1cfef49bd97f2338824d3f7572660774213811
Melih Darcan <57872471+MelihDarcanxyz@users.noreply.github.com>
2023-02-15 14:21:46 +0300

    Syntax highlighting theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,23 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
+// Shiki syntax-highlighting theme
+import catppuccin_mocha from "./src/themes/catppuccin-mocha.json";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.org/",
-  integrations: [mdx(), robotsTxt(), sitemap()]
+  integrations: [mdx(), robotsTxt(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: catppuccin_mocha,
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: ['python'],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
 });
