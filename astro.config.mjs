59f2b30408b64af71777c3bd32b268e6e0f31cda
Melih Darcan <57872471+MelihDarcanxyz@users.noreply.github.com>
2023-04-18 13:22:41 +0300

    feat: enable pagefind.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,17 +9,17 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
-// Shiki syntax-highlighting theme
-import catppuccin_mocha from "./src/themes/catppuccin-mocha.json";
+// https://github.com/shishkin/astro-pagefind
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:3000/",
-  integrations: [mdx(), robotsTxt(), sitemap()],
+  integrations: [mdx(), robotsTxt(), sitemap(), pagefind()],
   markdown: {
     shikiConfig: {
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: catppuccin_mocha,
+      theme: 'rose-pine-moon',
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
